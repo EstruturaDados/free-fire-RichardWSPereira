24cad085e075f1de53e94eb2c0771661088cda31
RichardWSPereira <srichardwilliam@hotmail.com>
2025-10-30 10:35:41 +0000

    NIVEL AVENTUREIRO: FreeFire

--- FreeFire.c
+++ FreeFire.c
@@ -2,8 +2,9 @@
 #include <string.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include <locale.h>
 
-// Código da Ilha – Edição Free Fire
+// Código da Ilha ? Edição Free Fire
 // Nível: Mestre
 // Este programa simula o gerenciamento avançado de uma mochila com componentes coletados durante a fuga de uma ilha.
 
@@ -45,15 +46,18 @@ void limpa_quebra_linha(char *string) {
 // Função para inserir um item
 void inseriritem(Inventario *inv);
 
-
 // Função para remover um item
 void removeritem(Inventario *inv);
-
 
 // Função para listar um item
 void listarItens(const Inventario *inv);
 
+// Função Buscar item por nome
+void buscaritem(Inventario *inv);
+
 int main() {
+    setlocale(LC_ALL,"Portuguese_Brazil");
+    
     // Criação da variável da struct Inventario
     Inventario inventario;
 
@@ -76,7 +80,8 @@ int main() {
         printf("\n");        
         printf("1. Adicionar um Item (Loot) \n");        
         printf("2. Remover Item\n");        
-        printf("3. Listar itens na Mochila \n");        
+        printf("3. Listar Itens na Mochila \n"); 
+        printf("4. Buscar Item por Nome \n");        
         printf("0. Sair\n");
         printf("-----------------------------\n");
         printf("Escolha uma opção: ");
@@ -86,7 +91,7 @@ int main() {
         // Ou digitar um numero maior que o numero de opções (3)
         // Ou digitar um numero menor que o numero de opções (0)
         // O programa notificará o erro
-        while (scanf("%d", &opcao) != 1 || opcao > 3 || opcao < 0 ) {
+        while (scanf("%d", &opcao) != 1 || opcao > 4 || opcao < 0 ) {
             liberarBuffer(); // Limpa o buffer
             printf("ERRO: Opção inexistente! Escolha novamente: ");
         }
@@ -111,6 +116,14 @@ int main() {
             case 3:
                 listarItens(&inventario);
                 break;
+            case 4:
+                printf(" --- Buscar Item na Mochila --- \n");
+                buscaritem(&inventario);
+                printf("Pressione ENTER para continuar... ");
+                // Comando que espera o usuario para prosseguir a execução do programa com o ENTER
+                getchar();
+                break;
+            break;
             case 0:
                 printf("Saindo...\n");
                 break;
@@ -124,7 +137,6 @@ int main() {
 }
 
 // Implementação das funções:
-
 
 // inserirItem():
 // Adiciona um novo componente à mochila se houver espaço.
@@ -234,4 +246,48 @@ void listarItens(const Inventario *inv) {
                inv->itens[i].quantidade);
     }
     printf("-------------------------------------------------------------\n");
+}
+
+void buscaritem(Inventario *inv) {
+    // Tratamento de erros para o caso de o inventario não ter itens para buscar
+    if (inv->numeroItens == 0) {
+        printf("Inventario vazio!\n");
+        return;
+    }
+
+    // variavel com a entrada do usuário do item que queremos buscar
+    char nomeItem[TAM_NOME];
+    printf("Digite o nome do item que deseja buscar: ");
+    fgets(nomeItem, TAM_NOME, stdin);
+    limpa_quebra_linha(nomeItem);
+
+    // Variável de inicialização da posição, quando encontrar o item esta variavel terá a posição do item desejado
+    int pos = -1;
+
+    // O for irá percorrer sequencialmente todo o vetor
+    for (int i = 0; i < inv->numeroItens;i++){
+
+        // COmando strcmp vai comparar o indice vetor com o nome do item desejado
+        // Encontrando, mudará a variavel pos, atribuindo o indice do item desejado
+        if (strcmp(inv->itens[i].nome, nomeItem) == 0) {
+            pos = i;
+            break;
+        }
+    }
+
+    // Caso a função não encontre o item com o nome igual ao do usuario
+    // A pos continuará sendo a que inicializou, logo, o item não foi encontrado
+    if (pos == -1) {
+        printf("Resultado: Item \"%s\" NÃO foi encontrado na mochila.\n", nomeItem);
+        return;
+    }
+
+    // A partir do momento que o item for encontrado
+    // Este printf imprimir o nome, tipo e quantidade do item desejado
+    printf("\n--- Item Encontrado! --- \n");
+    printf("Nome: \"%s\".\n", inv->itens[pos].nome);
+    printf("Tipo: \"%s\".\n", inv->itens[pos].tipo);
+    printf("Quantidade: \"%d\".\n", inv->itens[pos].quantidade);
+    printf("-------------------------\n");
+    printf("\n");
 }
